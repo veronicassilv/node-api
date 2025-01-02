719fb6fc940083afb67f2a48e517ddf745b2df10
veronicass97 <87318060+veronicass97@users.noreply.github.com>
2025-01-02 10:29:54 -0300

    refactor: move a sut creation to a facotru helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 import { MissingParamError } from "../errors/missing-param-error";
 import { SignUpController } from "./signup";
 
+const makeSut = (): SignUpController => {
+    return new SignUpController()
+}
+
 describe('SignUp Controller', () => {
     test('Should return 400 if no name is provided', () => {
-        const sut = new SignUpController();
+        const sut = makeSut()
         const httpRequest = {
             body: {
                 email: 'teste@gmail.com',
@@ -17,7 +21,7 @@ describe('SignUp Controller', () => {
     })
 
     test('Should return 400 if no email is provided', () => {
-        const sut = new SignUpController();
+        const sut = makeSut()
         const httpRequest = {
             body: {
                 name: 'Teste',
